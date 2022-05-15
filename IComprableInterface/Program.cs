696989cf34daee46ad67ab236937145e52f47df6
Marlon Salles <marlonsalles@hotmail.com>
2022-05-15 16:28:37 -0300

    Lendo um arquivo e salvando os nomes em uma lista

---
+++ IComprableInterface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace IComprableInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Temp\nomes.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) // Lendo o arquivo
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine()); //Adicionando as linhas em uma posicao no vetor
+                    }
+                    list.Sort();
+                    foreach (var name in list)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                string a = Console.ReadLine();
+            }
+
+        }
+    }
+}
