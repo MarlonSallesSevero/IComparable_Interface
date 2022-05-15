bdde048a1062c739b649ef98a5ecdd1161628e0a
Marlon Salles <marlonsalles@hotmail.com>
2022-05-15 16:59:00 -0300

    Implementando a Interface IComparable

--- IComprableInterface/Entities/Employee.cs
+++ IComprableInterface/Entities/Employee.cs
@@ -1,7 +1,18 @@
-﻿using System.Globalization;
+﻿/* 
+ *Less than zero	The current instance precedes the object specified by the CompareTo method 
+in the sort order.
+ 
+ *Zero	This current instance occurs in the same position in the sort order as the object specified
+by the CompareTo method.
+
+ *Greater than zero	This current instance follows the object specified by the CompareTo method in 
+the sort order.*/
+
+using System;
+using System.Globalization;
 namespace Entities
 {
-    internal class Employee
+    internal class Employee : IComparable
     {
         public string name { get; set; }
         public double salario { get; set; }
@@ -16,6 +27,16 @@ namespace Entities
         public override string ToString()
         {
             return $"{name}, {salario}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Obj is not a Employee");
+            }
+            Employee other = obj as Employee; //down casting
+            return salario.CompareTo(other.salario); //compareTo o atual com o other
         }
     }
 }
